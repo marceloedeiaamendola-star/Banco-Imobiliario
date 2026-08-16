35e738a1ddc9427e2fdc78c2358e9ddc97dbe9cb
marceloedeiaamendola-star <marceloedeia.amendola@gmail.com>
2026-08-15 21:43:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="banco-imobiliario-v4-fixed";
+const CACHE="banco-imobiliario-v4-definitiva";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
